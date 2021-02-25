bff77f4eca3214e0f4776f07fdfd4e23221ccf30
Bars <barsbekclash@gmail.com>
2021-02-25 17:13:46 +0600

    Refresh views.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,4 +4,26 @@ from django.shortcuts import render
 
 def index_view(request):
     modern = Modern.objects.all()
-    return render(request, 'index.html', context={'modern': modern}) 
+    return render(request, 'index.html', context={'modern': modern})
+
+def article_view(request):
+    modern_id = request.GET.get('id')
+    modern = Modern.objects.get(id=modern_id)
+    return render(request, 'article_view.html', context={'modern': modern})
+
+def article_create_view(request):
+    if request.method == "GET":
+        return render(request, 'modern_create.html')
+    elif request.method == "POST":
+        title = request.POST.get("title")
+        status = request.POST.get("status")
+        time = request.POST.get("time")
+
+        modern = Modern.objects.create(
+            title=title,
+            status=status,
+            time=time
+        )
+
+        return render(request, 'modern_view.html', context={'modern': modern})
+
