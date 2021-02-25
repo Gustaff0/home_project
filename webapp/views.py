4ed36436dc5a4b804cbf4a62ff8a7e62b9715191
Bars <barsbekclash@gmail.com>
2021-02-25 17:35:32 +0600

    refresh templates add 3 html and views

--- webapp/views.py
+++ webapp/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_view(request):
-    modern = Modern.objects.all()
-    return render(request, 'index.html', context={'modern': modern})
+    moderns = Modern.objects.all()
+    return render(request, 'index.html', context={'moderns': moderns})
 
 def modern_view(request):
     modern_id = request.GET.get('id')
     modern = Modern.objects.get(id=modern_id)
-    return render(request, 'article_view.html', context={'modern': modern})
+    return render(request, 'modern_view.html', context={'modern': modern})
 
 def modern_create_view(request):
     if request.method == "GET":
